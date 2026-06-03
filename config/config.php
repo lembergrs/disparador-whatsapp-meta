b66641b2dbc2d5ea921f8fe5b8c87930371112c8
RodrigoLemberg <rodrigo.lemberg@funarpen.com.br>
2026-06-03 15:36:16 -0300

    Arquivo de conexão

--- config/config.php
+++ config/config.php
@@ -4,6 +4,7 @@ define('BASE_URL', 'http://disparador.test');
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'whatsapp_disparador');
-define('DB_USER', 'root');
+define('DB_USER', 'root'); 
 define('DB_PASS', '');
 define('DB_PORT', '3306');
+
