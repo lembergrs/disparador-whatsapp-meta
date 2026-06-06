f47f5679e0c4454bfe4c0e70ed85020664af3589
Rodrigo Lemberg <lembergrs@gmail.com>
2026-06-06 10:30:50 -0300

    Tentando ajustar o load do site.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config/config.php';
 
-use Core\Router;
+use App\Core\Router;
 
 if (empty($_GET['url'])) {
     $_GET['url'] = 'site';
