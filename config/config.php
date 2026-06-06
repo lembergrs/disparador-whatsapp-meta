cd88dab4ba293439570e5afc4aca30941dcbb313
Rodrigo Lemberg <lembergrs@gmail.com>
2026-06-06 09:46:24 -0300

    Tentando resolver 1

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ define('BASE_URL', 'http://disparador.test');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'whatsapp_disparador');
 define('DB_USER', 'root'); 
-define('DB_PASS', ''); // teste
+define('DB_PASS', ''); // teste1
 define('DB_PORT', '3306');
 */
 
