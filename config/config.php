75a05332552617a7e797487df52985a837ecaa2d
Rodrigo Lemberg <lembergrs@gmail.com>
2026-06-05 23:50:36 -0300

    Mais um teste de deploy

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ define('BASE_URL', 'http://disparador.test');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'whatsapp_disparador');
 define('DB_USER', 'root'); 
-define('DB_PASS', '');
+define('DB_PASS', ''); // teste
 define('DB_PORT', '3306');
 */
 
