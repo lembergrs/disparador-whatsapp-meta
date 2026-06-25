98246944e7e48ea34cea0043a12db9bf3f4fc834
lembergrs <lembergrs@gmail.com>
2026-06-25 15:33:24 -0300

    Integrar clientes e cobrancas Asaas

--- app/Views/financeiro/index.php
+++ app/Views/financeiro/index.php
@@ -61,12 +61,29 @@ $avaliacao = \Core\Auth::dadosAvaliacaoCliente();
             </strong>
         </p>
 
-        <p class="mb-0">
+        <p class="mb-1">
             Vencimento:
             <strong>
                 <?= date('d/m/Y', strtotime($cobranca['COB_DataVencimento'])); ?>
             </strong>
         </p>
+
+        <p class="mb-<?= !empty($cobranca['COB_LinkPagamento']) ? '2' : '0'; ?>">
+            Status:
+            <strong><?= htmlspecialchars($cobranca['COB_Status']); ?></strong>
+        </p>
+
+        <?php if(!empty($cobranca['COB_LinkPagamento'])){ ?>
+            <a
+            href="<?= htmlspecialchars($cobranca['COB_LinkPagamento']); ?>"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="btn btn-success"
+            >
+                <i class="fas fa-credit-card"></i>
+                Pagar agora
+            </a>
+        <?php } ?>
 
     </div>
 
