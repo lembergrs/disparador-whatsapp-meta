f28aff84cb6758e84fb0fbc9041f8522e47cced5
lembergrs <lembergrs@gmail.com>
2026-08-07 12:11:56 -0300

    fix: organizar logging do task scheduler

--- processar_tarefas.php
+++ processar_tarefas.php
@@ -9,13 +9,19 @@ use Services\Tasks\TaskDispatcher;
 use Services\Tasks\TaskExecutionService;
 use Services\Tasks\TaskProcessor;
 use Services\Tasks\TaskRegistry;
+use Services\Tasks\TaskSchedulerCliOutput;
+use Services\Tasks\TaskSchedulerLogger;
+
+$verbose = in_array('--verbose', $argv ?? [], true);
+$saida = new TaskSchedulerCliOutput();
+$logger = new TaskSchedulerLogger(TASK_SCHEDULER_LOG_FILE);
 
 $lockPath = TASK_SCHEDULER_LOCK_FILE;
 $diretorioLock = dirname($lockPath);
 if(!is_dir($diretorioLock)) mkdir($diretorioLock, 0770, true);
 $lock = fopen($lockPath, 'c+');
 if(!$lock || !flock($lock, LOCK_EX | LOCK_NB)){
-    fwrite(STDERR, "Task Scheduler já está em execução.\n");
+    if($verbose) fwrite(STDERR, "Task Scheduler já está em execução.\n");
     exit(0);
 }
 
@@ -26,22 +32,19 @@ $liberar = function() use (&$lock, $lockPath){
 register_shutdown_function($liberar);
 
 try{
-    $logFile = TASK_SCHEDULER_LOG_FILE;
-    if(!is_dir(dirname($logFile))) mkdir(dirname($logFile), 0770, true);
-    $logger = function(array $linha) use ($logFile){
-        error_log(json_encode($linha, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . PHP_EOL, 3, $logFile);
-    };
     $repositorio = new TarefaAgendada();
     $processador = new TaskProcessor($repositorio,new TaskDispatcher(new TaskRegistry()),null,TASK_SCHEDULER_LEASE_MINUTES,$logger);
     $execucao = new TaskExecutionService($processador, TASK_SCHEDULER_BATCH_SIZE);
     $resumo = $execucao->processarSobDemanda(TASK_SCHEDULER_BATCH_SIZE);
-    echo 'Processadas: ' . $resumo['processadas'] . PHP_EOL;
-    echo 'Concluídas: ' . $resumo['concluidas'] . PHP_EOL;
-    echo 'Retry: ' . $resumo['retry'] . PHP_EOL;
-    echo 'Falhas: ' . $resumo['falhas'] . PHP_EOL;
-    $exitCode = $resumo['falhas'] > 0 ? 1 : 0;
+    $texto = $saida->formatar($resumo, $verbose);
+    if($texto !== '') echo $texto;
+    $exitCode = 0;
 }catch(Throwable $e){
-    error_log('Task Scheduler: falha operacional controlada.');
+    try{
+        $logger->operacional('falha_operacional_scheduler');
+    }catch(Throwable $logError){
+        if($verbose) fwrite(STDERR, "Task Scheduler: falha operacional e falha ao registrar log.\n");
+    }
     $exitCode = 1;
 }
 
