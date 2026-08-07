204dcfd949b9070433fac9744d397987c675a73b
lembergrs <lembergrs@gmail.com>
2026-08-07 11:21:53 -0300

    feat: adicionar catálogo fechado de tarefas

---
+++ app/Services/Tasks/TaskRegistry.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Services\Tasks;
+
+class TaskRegistry
+{
+    private $tipos;
+
+    public function __construct(array $tipos = null)
+    {
+        $this->tipos = $tipos ?? ['teste_scheduler'=>TesteSchedulerHandler::class];
+    }
+
+    public function possui($tipo): bool
+    {
+        return is_string($tipo) && isset($this->tipos[$tipo]);
+    }
+
+    public function handler($tipo): TaskHandlerInterface
+    {
+        if(!$this->possui($tipo)) throw new \InvalidArgumentException('Tipo de tarefa não permitido.');
+        $classe = $this->tipos[$tipo];
+        $handler = is_object($classe) ? $classe : new $classe();
+        if(!$handler instanceof TaskHandlerInterface) throw new \LogicException('Handler de tarefa inválido.');
+        return $handler;
+    }
+
+    public function tipos(): array
+    {
+        return array_keys($this->tipos);
+    }
+}
