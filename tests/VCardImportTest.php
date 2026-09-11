854820e5b827a5ca6b6b54daa5395ba05426ccb7
lembergrs <lembergrs@gmail.com>
2026-09-11 10:40:37 -0300

    Amplia testes da importação VCF

--- tests/VCardImportTest.php
+++ tests/VCardImportTest.php
@@ -26,13 +26,18 @@ END:VCARD
 BEGIN:VCARD
 VERSION:4.0
 N:Souza;João;;Sr.;
-TEL;TYPE=cell:tel:+5541988882222
+item1.TEL;TYPE=cell;VALUE=uri:tel:+5541988882222
 TEL;TYPE=work:+55 41 3333-4444
 END:VCARD
 BEGIN:VCARD
 VERSION:2.1
 N;CHARSET=UTF-8;ENCODING=QUOTED-PRINTABLE:Oliveira;Jos=C3=A9;;;
 TEL;CELL:41977773333
+END:VCARD
+BEGIN:VCARD
+VERSION:3.0
+FN:Contato sem telefone
+EMAIL:semtelefone@example.com
 END:VCARD
 VCF;
 
@@ -45,15 +50,30 @@ try{
     @unlink($arquivo);
 }
 
-$assert(count($linhas) === 5, 'VCF deve gerar cabeçalho mais uma linha para cada telefone.');
+$assert(count($linhas) === 6, 'VCF deve gerar cabeçalho, uma linha por telefone e registro sem telefone para contabilizar ignorados.');
 $assert($linhas[0] === ['Nome', 'Telefone', 'Email'], 'Cabeçalho VCF deve ser compatível com o importador atual.');
 $assert($linhas[1][0] === 'Maria da Silva', 'FN deve ser usado como nome principal.');
 $assert($linhas[1][1] === '+55 (41) 99999-1111', 'Telefone deve ser preservado para normalização pelo importador.');
 $assert($linhas[1][2] === 'maria@example.com', 'E-mail deve ser preservado nos dados do contato.');
 $assert($linhas[2][0] === 'Sr. João Souza', 'N deve ser usado como fallback quando FN não existir.');
 $assert($linhas[2][1] === '+5541988882222', 'URI tel: deve ser removida do valor do telefone.');
+$assert($linhas[3][1] === '+55 41 3333-4444', 'Múltiplos telefones do mesmo contato devem ser processados separadamente.');
 $assert($linhas[4][0] === 'José Oliveira', 'Quoted-printable do vCard 2.1 deve ser decodificado.');
+$assert($linhas[5][1] === '', 'Contato sem telefone deve chegar ao importador para ser contabilizado como ignorado.');
 
+$arquivoInvalido = tempnam(sys_get_temp_dir(), 'vcf_invalid_');
+file_put_contents($arquivoInvalido, "arquivo sem vcard\n");
+$erroVcfInvalido = false;
+
+try{
+    VCard::ler($arquivoInvalido);
+}catch(\Exception $e){
+    $erroVcfInvalido = strpos($e->getMessage(), 'Nenhum contato válido') !== false;
+}finally{
+    @unlink($arquivoInvalido);
+}
+
+$assert($erroVcfInvalido, 'Arquivo sem blocos VCARD deve ser rejeitado.');
 $assert(strpos($upload, "'vcf'") !== false, 'Upload deve aceitar extensão VCF.');
 $assert(strpos($upload, "'text/vcard'") !== false, 'Upload deve aceitar MIME text/vcard.');
 $assert(strpos($controller, 'use Core\\VCard;') !== false, 'Controller deve carregar o parser VCard.');
